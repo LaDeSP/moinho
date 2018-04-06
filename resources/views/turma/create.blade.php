c77a4c9a80a665a2454de15db8273141dd4301f2
AlanLucasSC <AlanLucasSC2011@gmail.com>
2018-04-06 15:43:23 -0400

    Turma - New Layout

--- resources/views/turma/create.blade.php
+++ resources/views/turma/create.blade.php
@@ -1,49 +1,44 @@
-
-
-    <style type="text/css">
-        div.col-md-4{
-            margin-top: 14px;
-        }
-    </style>
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('content')
     
 
     <form method= "POST" action="{{ route('turma.store') }}" enctype="multipart/form-data">
         {{ csrf_field() }}
-        <div class="col-md-4">
-        <h1>Cadastrar Turma</h1>
-        <table>
-             <tr>
-                <td><label>Nome: </label></td>
-                <td><select name="turma" class="form-control">
+        <h1 class="text-warning">Cadastrar Turma</h1>
+        <div class="row">
+            <!-- Dados da Turma -->
+            <div class="col-md-4">
+                <!-- Nome da Turma -->
+                <label for="exampleFormControlInput1">Nome</label>
+                <select name="turma" class="form-control">
                     @foreach($nome as $turma) 
                         <option value="{{ $turma->id }}"> {{ $turma->nome_turma }} </option>
                     @endforeach
-                    </select> </td>
-            </tr>
-            <tr>
-                <td><label>Turno: </label></td>
-                <td><input type="text" name="turno" size="23" class="form-control"></td>
-            </tr>
-            <tr>
-                <td><label>Ano: </label></td>
-                <td><input type="year" name="ano" size="23" class="form-control"></td>
-            </tr>
-            <tr>
-                <td><label>Semestre: </label></td>
-                <td><input type="text" name="periodo" size="23" class="form-control"></td>
-            </tr>
-            <tr>
-                <td><input type="submit" class="btn-success"></td>
-            </tr>
-        </table>
-       
-    
-        </div>
-
-        
+                </select> 
+            </div>
+            <div class="col-md-4">
+                <!-- Turno da Turma -->
+                <label for="exampleFormControlInput1">Turno</label>
+                <input type="text" name="turno" size="23" class="form-control">
+            </div>
+            <div class="col-md-4">
+                <!-- Ano da Turma -->
+                <label for="exampleFormControlInput1">Ano</label>
+                <input type="year" name="ano" size="23" class="form-control">
+            </div>
+            <div class="col-md-4">
+                <!-- Semestre da Turma -->
+                <label for="exampleFormControlInput1">Semestre</label>
+                <input type="text" name="periodo" size="23" class="form-control">
+            </div>
+            <div class="col-md-8">
+            </div>
+            <div class="col-md-4">
+                <!-- Submit -->
+                <button type="submit" class="btn btn-outline-info" id="submit">Adicionar</button>
+            </div>
+        </div>   
     </form>
 @endsection
 
