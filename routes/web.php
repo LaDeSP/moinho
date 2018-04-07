c66dc8cd5f8ce1a9c7b89aa9b9862b2ca6c9209b
KelvynBarros <kelvyn-1@hotmail.com>
2018-04-07 17:38:26 -0400

    Atribuindo o idioma à variável de sessão
    Vamos criar uma rota que captura a solicitação de cada link que nos permite atribuir o idioma que queremos para a variável de sessão "lang".

--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,69 @@
 |
 */
 
+Route::group(['middleware' => ['web']], function () {
+    
+   Route::get('lang/{lang}', function ($lang) {
+        session(['lang' => $lang]);
+        App::setLocale($lang);
+        return \Redirect::back();
+    });
+});
+
 Route::get('/', function () {
     return view('auth.login');
 });
 
 Route::group(['middleware' => ['auth']], function () {//trocar web pra auth quando for colocar restrição
-    //Resource
     Route::resource('dados_inscricao', 'dados_inscricaoController');
-    Route::resource('escola', 'escolaController');
-    Route::resource('matricula', 'matriculaController');
-    Route::resource('NomeTurma', 'nome_turmaController');
-    Route::resource('turma', 'turmaController');
-    Route::resource('listar_matriculas', 'listar_matriculasController');
-    Route::resource('lista_matriculas_irregulares', 'lista_matriculas_irregularesController');
-    Route::resource('colaborador', 'colaboradorController');
-    Route::resource('disciplina', 'disciplinaController');
-    Route::resource('turma_disciplina', 'turma_disciplinaController');
-    Route::resource('documento', 'documentoController');
-
-    //Get
-    Route::get('mostra_regulares', 'listar_matriculasController@index');
-    Route::get('mostra_irregulares', 'lista_matriculas_irregularesController@index');
-    Route::get('/matricula', 'listar_matriculasController@matricula');
-    Route::get('/pessoa', 'listar_matriculasController@pessoa');
-    Route::get('/ajudinha', 'listar_matriculasController@ajudinha');
-    Route::get('/inscricao', 'listar_matriculasController@inscricao');
-    Route::get('/dados', 'listar_matriculasController@dados_inscricao');
-    Route::get('/nome_turma', 'listar_matriculasController@nome_turma');
-    Route::get('/relatorio_inscricao', 'relatorioController@export');
-    Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('auth/logout', 'Auth\LoginController@logout');
+   // Route::post('dados_inscricao/busca', 'dados_inscricaoController@pesquisa');
 });
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('escola', 'escolaController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('matricula', 'matriculaController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('NomeTurma', 'nome_turmaController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('turma', 'turmaController');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('listar_matriculas', 'listar_matriculasController');
+    Route::get('mostra_regulares', 'listar_matriculasController@index');
+
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('lista_matriculas_irregulares', 'lista_matriculas_irregularesController');
+    Route::get('mostra_irregulares', 'lista_matriculas_irregularesController@index');
+
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('colaborador', 'colaboradorController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('disciplina', 'disciplinaController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('turma_disciplina', 'turma_disciplinaController');
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('documento', 'documentoController');
+});
+  Route::get('/matricula', 'listar_matriculasController@matricula');
+  Route::get('/pessoa', 'listar_matriculasController@pessoa');
+    Route::get('/ajudinha', 'listar_matriculasController@ajudinha');
+   Route::get('/inscricao', 'listar_matriculasController@inscricao');
+   Route::get('/dados', 'listar_matriculasController@dados_inscricao');
+  // Route::get('/turma', 'listar_matriculasController@turma');
+   Route::get('/nome_turma', 'listar_matriculasController@nome_turma');
 
 
 Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/logout', 'Auth\LoginController@logout');
