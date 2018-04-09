9627928fea51a454b27bfe3783ae304092710ef2
AlanLucasSC <AlanLucasSC2011@gmail.com>
2018-04-09 15:27:55 -0400

    Novo model - participante

---
+++ app/Participante.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Participante extends Model
+{
+    protected $table = 'participante';
+    
+    public $timestamps = false;
+
+}
