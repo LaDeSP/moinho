e3eda5ce619c23b6635a974eee47eaec246410fd
KelvynBarros <kelvyn-1@hotmail.com>
2018-04-09 21:09:52 -0400

    Add files via upload

--- resources/views/turma/create.blade.php
+++ resources/views/turma/create.blade.php
@@ -5,12 +5,12 @@
 
     <form method= "POST" action="{{ route('turma.store') }}" enctype="multipart/form-data">
         {{ csrf_field() }}
-        <h1 class="text-warning">Adicionar Turma</h1>
+        <h1 class="text-warning"><?php echo Lang::get('conteudo.addClass');?></h1>
         <div class="row">
             <!-- Dados da Turma -->
             <div class="col-md-4">
                 <!-- Nome da Turma -->
-                <label for="exampleFormControlInput1">Nome</label>
+                <label for="exampleFormControlInput1"><?php echo Lang::get('validation.attributes.name'); ?></label>
                 <select name="turma" class="form-control">
                     @foreach($nome as $turma) 
                         <option value="{{ $turma->id }}"> {{ $turma->nome_turma }} </option>
@@ -19,24 +19,24 @@
             </div>
             <div class="col-md-4">
                 <!-- Turno da Turma -->
-                <label for="exampleFormControlInput1">Turno</label>
+                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.shift');?> </label>
                 <input type="text" name="turno" size="23" class="form-control">
             </div>
             <div class="col-md-4">
                 <!-- Ano da Turma -->
-                <label for="exampleFormControlInput1">Ano</label>
+                <label for="exampleFormControlInput1"><?php echo Lang::get('validation.attributes.year'); ?></label>
                 <input type="year" name="ano" size="23" class="form-control">
             </div>
             <div class="col-md-4">
                 <!-- Semestre da Turma -->
-                <label for="exampleFormControlInput1">Semestre</label>
+                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.semester');?></label>
                 <input type="text" name="periodo" size="23" class="form-control">
             </div>
             <div class="col-md-8">
             </div>
             <div class="col-md-4">
                 <!-- Submit -->
-                <button type="submit" class="btn btn-outline-info" id="submit">Adicionar</button>
+                <button type="submit" class="btn btn-outline-info" id="submit"><?php echo Lang::get('conteudo.add');?></button>
             </div>
         </div>   
     </form>
