77d82e89258994588ba7d7f7ee63560f2bc90fb7
Alan Lucas Silva de Castro <alanlucassc2011@gmail.com>
2018-08-17 08:04:49 -0400

    Adicionar varias datas e horas na disciplina - feito

--- resources/views/disciplina/create.blade.php
+++ resources/views/disciplina/create.blade.php
@@ -60,8 +60,8 @@ $cont = 1;
             </div>
             <div class="col-md-4">
                 <!-- Dia da Semana da Disciplina -->
-                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.dayWeek'); ?>*</label>
-                <select name="dia_semana" class="form-control">
+                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.dayWeek'); ?></label>
+                <select name="dia_semana" class="form-control" id="dia_senama">
                     <option value="Segunda-Feira"> <?php echo Lang::get('conteudo.monday'); ?> </option>
                     <option value="Terça-Feira"> <?php echo Lang::get('conteudo.tuesday'); ?> </option>
                     <option value="Quarta-Feira"> <?php echo Lang::get('conteudo.wednesday'); ?> </option>
@@ -71,16 +71,29 @@ $cont = 1;
             </div>
             <div class="col-md-4">
                 <!-- Hora da Disciplina -->
-                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.hour'); ?>*</label>
-                <input type="time" name="hora" size="23" class="form-control validate"
-                id="hora" onkeyup="verifica_vazio(this.value, this.id);">
+                <label for="exampleFormControlInput1"><?php echo Lang::get('conteudo.hour'); ?></label>
+                <input type="time" name="hora" size="23" class="form-control"
+                id="hora">
                 <div class="invalid-feedback">
                     Por favor, digite a hora da disciplina
                 </div>
             </div>
+            <div class="col-md-12 text-right">
+                <!-- Submit -->
+                <button type="button" class="btn btn-outline-info" onClick="inserirDataHora('dia_senama', 'hora')"> Inserir Data e Hora </button>
+                <small class="form-text text-muted">Para adicionar uma disciplina é preciso inserir pelo menos uma data e hora.</small>
+            </div>
             <div class="col-md-4">
                 <!-- Submit -->
                 <button type="submit" class="btn btn-outline-danger" id="submit" disabled><?php echo Lang::get('conteudo.add'); ?></button>
+            </div>
+            <input type="text" class="validate d-none" id="validate">
+            <div class="col-md-12">
+                <!-- Horarios adicionados -->
+                <br>
+                <h3 class="text-info">Dia da semana e Hora selecionados</h3>
+                <div id="datas_horarios" class="row">
+                </div>
             </div>
         </div>
     </form>
@@ -157,6 +170,30 @@ $cont = 1;
 <script src="/vendor/jquery/jquery.min.js"></script>
 
 <script>
+    function inserirDataHora(data, hora){
+        hora = $('#'+hora).val();
+        data = $('#'+data).val();
+        if( hora ){
+            $('#datas_horarios').append(`
+                <div id=`+contador+` class="col-md-4 col-sm-6">
+                    <span href="#" class=" list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">`+data+`, `+hora+`</h5>
+                        </div>
+                        <input class="d-none" name="datas[]" type="text" value="`+data+`"/>
+                        <input class="d-none" name="horas[]" type="time" value="`+hora+`"/>
+                        <button type="button" onClick="excluirDataHora(`+contador+`)" class="btn btn-outline-success" >Excluir Horário</button>
+                    </span>
+                </div>
+            `);
+            contador++;
+        }
+        if( $('#datas_horarios').children().length ){
+            $('#validate').addClass("is-valid");
+            verifica_submit('validate');
+        }
+    }
+
     $(document).ready(function(){
         //paginacao( 20, 1 );
         paginacao( {{ $count }}, 24 );
